136e8e047ef6debe74c72a7c10906a0520b1e53f
elvxk <pradiptaramadan9c@gmail.com>
2024-10-25 17:51:30 +0700

    💡 [update] POST method

--- src/app/(api)/projects/route.js
+++ src/app/(api)/projects/route.js
@@ -1,7 +1,7 @@
 import { getAllProjects } from "./service";
 import { NextResponse } from "next/server";
 
-export async function GET(request) {
+export async function POST(request) {
   const projects = await getAllProjects();
   return NextResponse.json({ status: 200, message: "success", data: projects });
 }
--- src/app/(api)/v1/route.js
+++ src/app/(api)/v1/route.js
@@ -1,14 +1,3 @@
-import prisma from "@/libs/prisma";
-
-export default async function handler(req, res) {
-  if (req.method === "GET") {
-    try {
-      const projects = await prisma.project.findMany();
-      res.status(200).json(projects);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    } finally {
-      await prisma.$disconnect(); // Disconnect Prisma Client
-    }
-  }
+export async function GET(request) {
+  return new Response("Hii there, I am elvxk API v1");
 }
